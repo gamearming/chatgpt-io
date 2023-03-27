120a9a6ec514a1a5c3d1585df30bb3169c49c891
gamearming <gamearming@hotmail.com>
2023-03-28 01:28:28 +0800

    加入繁體中文註解

--- src/enums/error-type.ts
+++ src/enums/error-type.ts
@@ -1,11 +1,40 @@
-enum ErrorType {
-	Timeout = 1,
-	AccountRateLimitExceeded = 2,
-	AnotherMessageInProgress = 3,
-	SessionTokenExpired = 4,
-	MessageTooLong = 5,
-	ConversationNotFound = 6,
-	UnknownError = 7,
+非常抱歉，我犯了錯誤。以下是已修正的程式碼註釋:
+
+```
+/**
+ * 定義錯誤類型的列舉型別。
+ * @enum {number}
+ */
+ enum ErrorType {
+    /**
+     * 超時錯誤。
+     */
+    Timeout = 1,
+    /**
+     * 帳號速率限制超過錯誤。
+     */
+    AccountRateLimitExceeded = 2,
+    /**
+     * 另一條訊息正在進行中錯誤。
+     */
+    AnotherMessageInProgress = 3,
+    /**
+     * 對話金鑰過期錯誤。
+     */
+    SessionTokenExpired = 4,
+    /**
+     * 訊息過長錯誤。
+     */
+    MessageTooLong = 5,
+    /**
+     * 找不到對話錯誤。
+     */
+    ConversationNotFound = 6,
+    /**
+     * 未知錯誤。
+     */
+    UnknownError = 7,
 }
 
 export default ErrorType;
+```
