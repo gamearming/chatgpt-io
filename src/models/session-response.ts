87d30aa0aeb4ef743f1b4178d12582c563f87fab
gamearming <gamearming@hotmail.com>
2023-03-28 01:25:15 +0800

    加入繁體中文註解

--- src/models/session-response.ts
+++ src/models/session-response.ts
@@ -1,7 +1,23 @@
+/**
+ * SessionResponse 界面定義當從對話機器人獲取對話回應時，將使用哪些屬性來構建回答。
+ * @interface
+ */
 interface SessionResponse {
-	sessionToken: string;
-	auth: string;
-	expires: string;
+    /**
+     * 對話金鑰。
+     * @type {string}
+     */
+    sessionToken: string;
+    /**
+     * 授權。
+     * @type {string}
+     */
+    auth: string;
+    /**
+     * 過期時間。
+     * @type {string}
+     */
+    expires: string;
 }
 
 export default SessionResponse;
