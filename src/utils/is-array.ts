b0b0c79a15c5a250828e808070e7c3cd80b233ef
gamearming <gamearming@hotmail.com>
2023-03-28 01:11:19 +0800

    加入繁體中文註解

--- src/utils/is-array.ts
+++ src/utils/is-array.ts
@@ -1,5 +1,10 @@
+/**
+ * 判斷一個變數是否為陣列。
+ * @param {*} a 要檢查的變數。
+ * @returns {boolean} 如果傳入的變數是陣列，返回 true；否則，返回 false。
+ */
 function isArray(a: any) {
-	return !!a && a.constructor === Array;
+    return !!a && a.constructor === Array;
 }
 
 export default isArray;
