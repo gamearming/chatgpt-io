5f63257999d136a5f6c72f56a35a29938b589dd8
gamearming <gamearming@hotmail.com>
2023-03-28 01:20:24 +0800

    加入繁體中文註解

--- src/models/conversation.ts
+++ src/models/conversation.ts
@@ -1,8 +1,28 @@
+/**
+ * 定義對話的屬性。
+ * @interface
+ */
 interface Conversation {
-	id: string;
-	conversationId?: string;
-	parentId: string;
-	lastActive: number;
+    /**
+     * 對話 ID。
+     * @type {string}
+     */
+    id: string;
+    /**
+     * 上一個對話 ID。
+     * @type {string}
+     */
+    conversationId?: string;
+    /**
+     * 父對話 ID。
+     * @type {string}
+     */
+    parentId: string;
+    /**
+     * 最後活動時間。
+     * @type {number}
+     */
+    lastActive: number;
 }
 
 export default Conversation;
