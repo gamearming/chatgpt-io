2a44859b08396b902c10af70bde110096ece023e
gamearming <gamearming@hotmail.com>
2023-03-28 01:22:36 +0800

    加入繁體中文註解

--- src/models/options.ts
+++ src/models/options.ts
@@ -1,13 +1,41 @@
 import AccountType from "../enums/account-type.js";
 import LogLevel from "../enums/log-level.js";
 
+/**
+ * 定義選項介面。
+ * @interface
+ */
 interface Options {
-	name?: string;
-	logLevel?: LogLevel;
-	bypassNode?: string;
-	accountType?: AccountType;
-	configsDir?: string;
-	saveInterval?: number;
+    /**
+     * 名稱。
+     * @type {string}
+     */
+    name?: string;
+    /**
+     * 日誌級別。
+     * @type {LogLevel}
+    */
+    logLevel?: LogLevel;
+    /**
+     * 繞過節點的位址。
+     * @type {string}
+     */
+    bypassNode?: string;
+    /**
+     * 帳戶類型。
+     * @type {AccountType}
+     */
+    accountType?: AccountType;
+    /**
+     * 設定檔目錄。
+     * @type {string}
+     */
+    configsDir?: string;
+    /**
+     * 自動儲存時間間隔（以毫秒為單位）。
+     * @type {number}
+     */
+    saveInterval?: number;
 }
 
 export default Options;
