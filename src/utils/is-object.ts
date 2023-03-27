f6f13aaf4a3013d4e820eb1a9264e58a10ae25ec
gamearming <gamearming@hotmail.com>
2023-03-28 01:11:55 +0800

    加入繁體中文註解

--- src/utils/is-object.ts
+++ src/utils/is-object.ts
@@ -1,5 +1,10 @@
+/**
+ * 判斷一個變數是否為物件。
+ * @param {*} a 要檢查的變數。
+ * @returns {boolean} 如果傳入的變數是物件，返回 true；否則，返回 false。
+ */
 function isObject(a: any) {
-	return !!a && a.constructor === Object;
+    return !!a && a.constructor === Object;
 }
 
 export default isObject;
