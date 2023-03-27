af53a9bdfac803c74d327236b333a5405acd87e7
gamearming <gamearming@hotmail.com>
2023-03-28 01:23:52 +0800

    加入繁體中文註解

--- src/models/result.ts
+++ src/models/result.ts
@@ -1,10 +1,31 @@
 import ErrorType from "../enums/error-type.js";
 
+/**
+ * 定義帶有結果的介面。
+ * @interface
+ * @template Type 泛型型別參數，表示傳回值的類型。
+ */
 interface Result<Type> {
-	status: boolean;
-	data: Type;
-	errorType: ErrorType;
-	error: string;
+    /**
+     * 狀態。
+     * @type {boolean}
+     */
+    status: boolean;
+    /**
+     * 數據。
+     * @type {Type}
+     */
+    data: Type;
+    /**
+     * 錯誤類型。
+     * @type {ErrorType}
+     */
+    errorType: ErrorType;
+    /**
+     * 錯誤訊息。
+     * @type {string}
+     */
+    error: string;
 }
 
 export default Result;
