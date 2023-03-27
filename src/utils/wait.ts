b31ec783d035b31c61da023dbac3104d4ce6b654
gamearming <gamearming@hotmail.com>
2023-03-28 01:15:47 +0800

    加入繁體中文註解

--- src/utils/wait.ts
+++ src/utils/wait.ts
@@ -1,7 +1,12 @@
+/**
+ * 暫停執行一段時間。
+ * @param {number} time 要暫停的時間，以毫秒為單位。
+ * @returns {Promise<void>} 當暫停完成時解析的 Promise。
+ */
 function wait(time: number) {
-	return new Promise((resolve) => {
-		setTimeout(resolve, time);
-	});
+    return new Promise((resolve) => {
+        setTimeout(resolve, time);
+    });
 }
 
 export default wait;
